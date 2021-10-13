d5ebbbaea02c0cda87b6a1abbe28f0c956fe9e11
JosferL <josfer.lumapas@resmed.com.au>
2021-10-13 05:04:32 +0000

    remove other variables

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,12 +2,5 @@ module "s3_bucket" {
   source = "app.terraform.io/hashicat-aws-jl/s3-bucket/aws"
   version = "2.2.0"
 
-  bucket = "my-s3-bucket"
-  acl    = "private"
   bucket_prefix = "var.prefix"
-
-  versioning = {
-    enabled = true
-  }
-
 }
